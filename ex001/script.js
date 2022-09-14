5a0f35fae7cb5b6d634633d2b9a765e4a47d0293
Amadeu Santos <amadeusantos31@gmail.com>
2022-09-14 08:58:48 -0300

    fist commit

---
+++ ex001/script.js
@@ -0,0 +1,17 @@
+// escreve mais 1 olá, mundo! apos um clique no button
+function escrever(){
+    var html = document.body;
+    html.innerHTML += "<p>olá mundo! </p></br>"
+}
+
+// pinta o fundo de azul
+function pintarBlue(){
+    var html = document.body;
+    html.style.background = "blue";
+}
+
+// pinta o fundo de branco
+function pintarWhite(){
+    var html = document.body;
+    html.style.background = "#ffffff";
+}
