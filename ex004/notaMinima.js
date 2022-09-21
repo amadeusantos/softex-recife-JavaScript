f7d6d2673e330aacd2709381a8467cba8755f591
Amadeu Santos <amadeusantos31@gmail.com>
2022-09-21 16:24:15 -0300

    Adicionando o operador ternario

--- ex004/notaMinima.js
+++ ex004/notaMinima.js
@@ -2,9 +2,5 @@ function notaMinima() {
     var falta = parseFloat(document.getElementById("valor4").value);
     falta += parseFloat(document.getElementById("valor5").value);
     falta = 21 - falta
-    if (falta <= 10){
-        alert(`Sua proxima nota tem que ser ${falta} para passar.`);
-    } else {
-        alert("Você já está reprovado! Faz um boa prova e uma boa recuperação.")
-    }
+    alta <= 10 ? alert(`Sua proxima nota tem que ser ${falta} para passar.`): alert("Você já está reprovado! Faz um boa prova e uma boa recuperação.");
 }
