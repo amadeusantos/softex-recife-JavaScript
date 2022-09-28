0d61a14f46527ef6142cde9fbc081087bf836f28
Amadeu Santos <amadeusantos31@gmail.com>
2022-09-28 14:39:02 -0300

    Usando function paramentos e arrow function

---
+++ ex006/script.js
@@ -0,0 +1,11 @@
+function helloWorld() {
+    console.log("Hello World!");
+}
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+var circulo = (raio) => {console.log(raio ** 2 * Math.PI)};
+console.log("Área do circulo de raio 1.5:")
+circulo(1.5)
